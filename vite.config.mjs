f9ca492b33835a755fbdfbfe9ddfe33929645dff
yhosutun2490 <yhosutun2491@gmail.com>
2024-11-05 16:00:36 +0800

    <fix> 修正打包設定

--- vite.config.mjs
+++ vite.config.mjs
@@ -36,7 +36,12 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['vuetify'] // 如果是第三方庫，排除該庫靜態優化
   },
-  define: { 'process.env': {} },
+  define: { 
+  'process.env': {}, 
+  '__VUE_OPTIONS_API__': true,
+  '__VUE_PROD_DEVTOOLS__': false,
+  '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false 
+},
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
