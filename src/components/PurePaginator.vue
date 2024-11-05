f1d02c6a5020426dbab9e7d3646d6ad827391e2b
yhosutun2490 <yhosutun2491@gmail.com>
2024-11-05 13:56:31 +0800

    <fix> 客製參數改用props傳遞

--- src/components/PurePaginator.vue
+++ src/components/PurePaginator.vue
@@ -1,10 +1,16 @@
 <template>
   <div v-if="true">
-    <v-pagination :length="5" ref="paginationRef"></v-pagination>
+    <v-pagination :length="length" ref="paginationRef"></v-pagination>
   </div>
 
 </template>
 <script setup>
-import {ref} from 'vue'
+import {ref, defineProps} from 'vue'
 const paginationRef = ref(null)
+const props  = defineProps({
+  length: {
+    type: Number,
+    default: 5
+  }
+})
 </script>
