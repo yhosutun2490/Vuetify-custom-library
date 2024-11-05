00e808603fecadb34ecb7f0a1e4e9bfc9904d321
yhosutun2490 <yhosutun2491@gmail.com>
2024-11-05 16:05:29 +0800

    <fix> 模組僅用具名導出

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import PurePaginator from "./components/PurePaginator.vue";
 import PureButton from "./components/PureButton.vue";
 import { registerPlugins } from '@/plugins' // vuetify 設定
 // 添加 install 方法
-export default  {
-  install: (app)=> {
-    registerPlugins(app)
-    app.component('Paginator',Paginator)
-    app.component('PurePaginator',PurePaginator)
-    app.component('PureButton',PureButton)
-  }
-}
+// export default  {
+//   install: (app)=> {
+//     registerPlugins(app)
+//     app.component('Paginator',Paginator)
+//     app.component('PurePaginator',PurePaginator)
+//     app.component('PureButton',PureButton)
+//   }
+// }
 
 export {registerPlugins,Paginator,PurePaginator,PureButton}
